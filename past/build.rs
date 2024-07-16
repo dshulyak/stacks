fd726088c047fdd499688dd17c90732f328c9ed0
Dmitry Shulyak <yashulyak@gmail.com>
2024-07-16 07:52:23 +0200

    there is no need to commit past.skel.rs

--- past/build.rs
+++ past/build.rs
@@ -7,10 +7,7 @@ use libbpf_cargo::SkeletonBuilder;
 const SRC: &str = "src/bpf/past.bpf.c";
 
 fn main() -> Result<()> {
-    let out = PathBuf::from(env::var_os("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR must be set in build script"))
-        .join("src")
-        .join("bpf")
-        .join("past.skel.rs");
+    let out = PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR must be set in build script")).join("past.skel.rs");
 
     let arch = env::var("CARGO_CFG_TARGET_ARCH").expect("CARGO_CFG_TARGET_ARCH must be set in build script");
     SkeletonBuilder::new()
