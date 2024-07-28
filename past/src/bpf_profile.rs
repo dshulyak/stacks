483294ecb115b118fcfcf8d5ffcc6d964e5a0f7b
Dmitry Shulyak <yashulyak@gmail.com>
2024-07-28 10:41:21 +0200

    dump periodic profiles into info log

--- past/src/bpf_profile.rs
+++ past/src/bpf_profile.rs
@@ -1,9 +1,12 @@
 use std::{
+    collections::HashMap,
+    fmt::Display,
     mem::size_of_val,
     os::{
         fd::{AsFd, AsRawFd, FromRawFd, OwnedFd},
         raw::c_void,
     },
+    time,
 };
 
 use libbpf_rs::{
@@ -11,8 +14,14 @@ use libbpf_rs::{
     libbpf_sys::{bpf_enable_stats, bpf_obj_get_info_by_fd, bpf_prog_info, BPF_STATS_RUN_TIME},
     Program, Result,
 };
+use tracing::info;
 
-pub(crate) fn enable() -> Result<StatsFd> {
+use crate::{
+    bpf::{get_program, ProgramName},
+    PastProgs,
+};
+
+fn enable() -> Result<StatsFd> {
     let fd = unsafe { bpf_enable_stats(BPF_STATS_RUN_TIME) };
     if fd < 0 {
         Err(libbpf_rs::Error::from_raw_os_error(fd))
@@ -24,19 +33,13 @@ pub(crate) fn enable() -> Result<StatsFd> {
 }
 
 #[derive(Debug)]
-pub(crate) struct Stats {
-    pub(crate) name: String,
-    pub(crate) runtime_ns: u64,
-    pub(crate) runtime_cnt: u64,
-}
-
-#[derive(Debug)]
-pub(crate) struct StatsFd {
+struct StatsFd {
     _fd: OwnedFd,
 }
 
 impl StatsFd {
-    pub(crate) fn get_stats(&self, prog: &Program) -> Result<Stats> {
+    // get_stats returns the runtime and run count of the program.
+    fn get_stats(&self, prog: &Program) -> Result<(u64, u64)> {
         let mut item = bpf_prog_info::default();
         let item_ptr: *mut bpf_prog_info = &mut item;
         let mut len = size_of_val(&item) as u32;
@@ -44,18 +47,129 @@ impl StatsFd {
         if rst < 0 {
             return Err(libbpf_rs::Error::from_raw_os_error(rst));
         }
-        Ok(Stats {
-            name: null_terminated_to_string(&item.name),
-            runtime_ns: item.run_time_ns,
-            runtime_cnt: item.run_cnt,
-        })
+        Ok((item.run_time_ns, item.run_cnt))
     }
 }
 
-fn null_terminated_to_string(name: &[i8; 16]) -> String {
-    let name = match name.iter().position(|&b| b == 0) {
-        Some(pos) => &name[..pos],
-        None => name,
-    };
-    String::from_utf8(name.iter().copied().map(|b| b as u8).collect()).unwrap()
+#[derive(Debug)]
+struct DeltaStats {
+    program: ProgramName,
+    // duration is a wallclock time between two collection.
+    // it will be usually configured to something like 10s or 10m.
+    duration: time::Duration,
+    // runtime_ns is a total runtime of the bpf program.
+    runtime_ns: u64,
+    // runtime_cnt is a number of events observed by bpf program.
+    runtime_cnt: u64,
+    // collected_cnt is a number of events collected after filtering.
+    // additionally there is a delay between the timepoint when they are observed and collected.
+    collected_cnt: u64,
+}
+
+impl Display for DeltaStats {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let duration = humantime::Duration::from(self.duration);
+        let runtime = humantime::Duration::from(time::Duration::from_nanos(self.runtime_ns));
+        let busy = (self.runtime_ns as f64 / self.duration.as_nanos() as f64) * 100.0;
+        let collected_latency = self.runtime_ns as f64 / self.collected_cnt as f64;
+        let program: &str = self.program.into();
+        writeln!(
+            f,
+            "{:<15} | {:<15.4} | {:<15.4} | {:<15.2} | {:<15} | {:<15} | {:<15.2}",
+            program,
+            duration.as_secs_f64(),
+            runtime.as_secs_f64(),
+            busy,
+            self.runtime_cnt,
+            self.collected_cnt,
+            collected_latency
+        )
+    }
+}
+
+struct MultiLineStats<'a>(&'a [DeltaStats]);
+
+impl<'a> Display for MultiLineStats<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        if self.0.is_empty() {
+            return Ok(());
+        }
+        writeln!(
+            f,
+            "{:<15} | {:<15} | {:<15} | {:<15} | {:<15} | {:<15} | {:<15}",
+            "program", "duration s", "runtime s", "busy %", "runtime cnt", "collected cnt", "collected latency ns"
+        )?;
+        for stat in self.0 {
+            stat.fmt(f)?;
+        }
+        Ok(())
+    }
+}
+
+pub(crate) struct ProgramStats {
+    runtime_ns: u64,
+    runtime_cnt: u64,
+    collected_cnt: u64,
+}
+
+pub(crate) struct Profiler {
+    fd: StatsFd,
+    interval: time::Duration,
+    last: time::Instant,
+    stats: HashMap<ProgramName, ProgramStats>,
+    events_per_program_counter: HashMap<ProgramName, u64>,
+    scratch: Vec<DeltaStats>,
+}
+
+impl Profiler {
+    pub(crate) fn enable(interval: time::Duration) -> Result<Self> {
+        Ok(Profiler {
+            fd: enable()?,
+            last: time::Instant::now(),
+            interval,
+            stats: HashMap::new(),
+            events_per_program_counter: HashMap::new(),
+            scratch: vec![],
+        })
+    }
+
+    pub(crate) fn collect(&mut self, name: ProgramName) {
+        let counter = self.events_per_program_counter.entry(name).or_insert(0);
+        *counter += 1;
+    }
+
+    pub(crate) fn log_stats_on_interval(&mut self, progs: &PastProgs) -> Result<()> {
+        let ts: time::Instant = time::Instant::now();
+        if ts.duration_since(self.last) < self.interval {
+            return Ok(());
+        }
+        for (name, collected_cnt) in self.events_per_program_counter.iter() {
+            let (runtime_ns, runtime_cnt) = self.fd.get_stats(get_program(*name, progs))?;
+            let previous = self.stats.entry(*name).or_insert(ProgramStats {
+                runtime_ns: 0,
+                runtime_cnt: 0,
+                collected_cnt: 0,
+            });
+
+            let delta = DeltaStats {
+                program: *name,
+                duration: ts.duration_since(self.last),
+                runtime_ns: runtime_ns - previous.runtime_ns,
+                runtime_cnt: runtime_cnt - previous.runtime_cnt,
+                collected_cnt: collected_cnt - previous.collected_cnt,
+            };
+
+            previous.runtime_ns = runtime_ns;
+            previous.runtime_cnt = runtime_cnt;
+            previous.collected_cnt = *collected_cnt;
+
+            self.scratch.push(delta);
+        }
+        self.last = ts;
+        if !self.scratch.is_empty() {
+            info!("BPF PROFILE:\n{}", MultiLineStats(self.scratch.as_slice()));
+            self.scratch.clear();
+        }
+        Ok(())
+    }
 }
--- past/src/main.rs
+++ past/src/main.rs
@@ -1,12 +1,19 @@
 use std::{
-    collections::HashSet, fs, io::Read, path::{Path, PathBuf}, sync::{
+    cell::RefCell,
+    collections::HashSet,
+    fs,
+    io::Read,
+    path::{Path, PathBuf},
+    sync::{
         atomic::{AtomicBool, Ordering},
         Arc,
-    }, time::Duration
+    },
+    time::Duration,
 };
 
 use anyhow::{Context, Result};
 use bpf::{link, Program, Programs};
+use bpf_profile::Profiler;
 use clap::Parser;
 use libbpf_rs::{MapFlags, RingBufferBuilder};
 use tracing::{error, info, info_span, level_filters::LevelFilter, warn};
@@ -120,6 +127,13 @@ examples:
     )]
     debug_bpf: bool,
 
+    #[clap(
+        long,
+        default_value = "30m",
+        help = "output additional information about how much time is spent in bpf programs. it will be disabled if zero"
+    )]
+    profiling_interval: humantime::Duration,
+
     #[clap(long, default_value = "false", help = "print version and exit")]
     version: bool,
 }
@@ -179,7 +193,7 @@ fn main() -> Result<()> {
 
     let (mut skel, _links) = link(
         &programs,
-        opt.usdt.iter(),
+        opt.usdt.as_slice(),
         opt.debug_bpf,
         opt.bpf_events,
         opt.bpf_stacks,
@@ -228,8 +242,21 @@ fn main() -> Result<()> {
     let mut dropped_counter = 0;
     let sleep_interval = opt.poll.into();
     let progs = skel.progs();
+    let mut profiler = if opt.profiling_interval.as_nanos() > 0 {
+        Some(RefCell::new(Profiler::enable(opt.profiling_interval.into())?))
+    } else {
+        None
+    };
     loop {
-        match consume_events(&mut program, &maps, &progs, &mut dropped_counter, &interrupt, sleep_interval) {
+        match consume_events(
+            &mut program,
+            &maps,
+            profiler.as_mut(),
+            &progs,
+            &mut dropped_counter,
+            &interrupt,
+            sleep_interval,
+        ) {
             Ok(_) => break,
             Err(ErrorConsume::DroppedEvents(dropped)) => {
                 warn!("program missed events {}. will need to reinitialize state", dropped);
@@ -266,18 +293,28 @@ enum ErrorConsume {
 }
 
 fn consume_events<Fr: Frames, Sym: Symbolizer>(
-    program: &mut state::State<Fr, Sym>,
+    state: &mut state::State<Fr, Sym>,
     maps: &PastMaps,
+    profiler: Option<&mut RefCell<Profiler>>,
     progs: &PastProgs,
     dropped_counter: &mut u64,
     interrupt: &Arc<AtomicBool>,
     poll_interval: Duration,
 ) -> Result<(), ErrorConsume> {
-    let stats_fd = bpf_profile::enable()?;
     let mut builder = RingBufferBuilder::new();
     builder.add(maps.events(), |buf: &[u8]| {
-        if let Err(err) = program.on_event(buf.into()) {
-            error!("failed to process event: {:?}", err);
+        let event: Received = match buf.try_into() {
+            Ok(buf) => buf,
+            Err(err) => {
+                error!("invalid event: {:?}", err);
+                return 1;
+            }
+        };
+        if let Some(profiler) = &profiler {
+            profiler.borrow_mut().collect(event.program_name());
+        }
+        if let Err(err) = state.on_event(event) {
+            error!("non-recoverable error on event: {:?}", err);
             1
         } else {
             0
@@ -309,14 +346,12 @@ fn consume_events<Fr: Frames, Sym: Symbolizer>(
             *dropped_counter = updated_dropped_counter;
             return Err(ErrorConsume::DroppedEvents(delta));
         }
-            let stats = stats_fd.get_stats(progs.handle__mm_trace_rss_stat())?;
-            info!(
-                "program {} runtime_ns: {} runtime_cnt: {}",
-                stats.name,
-                stats.runtime_ns,
-                stats.runtime_cnt
-            );
-        
+
+        if let Some(profiler) = &profiler {
+            if let Err(err) = profiler.borrow_mut().log_stats_on_interval(progs) {
+                warn!("profiler failing to logs: {:?}", err);
+            }
+        }
     }
 }
 
