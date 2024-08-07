75ed21c3ddd851c80cb91fcf3f536b08880eb91b
Dmitry Shulyak <yashulyak@gmail.com>
2024-08-07 08:15:17 +0200

    drop pprof helpers
    query file will have to be specified each time

--- stacks/build.rs
+++ stacks/build.rs
@@ -7,7 +7,8 @@ use libbpf_cargo::SkeletonBuilder;
 const SRC: &str = "src/bpf/stacks.bpf.c";
 
 fn main() -> Result<()> {
-    let out = PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR must be set in build script")).join("stacks.skel.rs");
+    let out =
+        PathBuf::from(env::var_os("OUT_DIR").expect("OUT_DIR must be set in build script")).join("stacks.skel.rs");
 
     let arch = env::var("CARGO_CFG_TARGET_ARCH").expect("CARGO_CFG_TARGET_ARCH must be set in build script");
     let mut builder = SkeletonBuilder::new();
--- stacksexport/src/main.rs
+++ stacksexport/src/main.rs
@@ -8,10 +8,6 @@ mod common;
 mod pprof;
 mod trace;
 
-const CPU_PPROF_SQL: &str = include_str!("sql/cpu_ustacks_for_command.sql");
-const OFFCPU_PPROF_SQL: &str = include_str!("sql/offcpu_stacks_for_command.sql");
-const RSS_PPROF_SQL: &str = include_str!("sql/ustack_rss_growth_for_command.sql");
-
 #[derive(Parser)]
 struct Opt {
     #[clap(subcommand)]
@@ -24,13 +20,22 @@ struct Opt {
 
 #[derive(Parser)]
 enum Command {
+    #[clap(
+        about = r#"generate pprof from any custom query. sql query is expected to return rows with 3 columns.
+    first column is a collection with triples (name - utf8 string, address - u64, offset - u64).
+    second column is a number of collected samples - i64.
+    third column can be any data that is useful for pprof, for example in offcpu profile it can be a total waiting time - u64.    
+    "#
+    )]
     Pprof {
         #[clap(short, long, global = true, default_value = "/tmp/pprof.pb")]
         destination: PathBuf,
         #[clap(short, long, global = true, help = "path to the binary file")]
         binary: Option<PathBuf>,
-        #[clap(subcommand)]
-        cmd: PprofCommand,
+        #[clap(index = 1, help = "file with sql query")]
+        query_file: PathBuf,
+        #[clap(short, long, help = "name of the process, as it is recorded in /proce/<pid>/comm")]
+        command: Option<String>,
     },
     Trace {
         #[clap(
@@ -45,38 +50,6 @@ enum Command {
     },
 }
 
-#[derive(Parser)]
-enum PprofCommand {
-    #[clap(aliases = &["c"], about = "samples of cpu profile")]
-    Cpu {
-        #[clap(index = 1, help = "name of the process, as it is recorded in /proce/<pid>/comm")]
-        command: String,
-    },
-    #[clap(aliases = &["o"], about = "waiting to be run. it can be IO, sychronization, time")]
-    Offcpu {
-        #[clap(index = 1, help = "name of the process, as it is recorded in /proce/<pid>/comm")]
-        command: String,
-    },
-    #[clap(aliases = &["r"], about = "rss profile. stacks are collected when page is requested in kernel")]
-    Rss {
-        #[clap(index = 1, help = "name of the process, as it is recorded in /proce/<pid>/comm")]
-        command: String,
-    },
-    #[clap(
-        about = r#"generate pprof from any custom query. sql query is expected to return rows with 3 columns.
-first column is a collection with triples (name - utf8 string, address - u64, offset - u64).
-second column is a number of collected samples - i64.
-third column can be any data that is useful for pprof, for example in offcpu profile it can be a total waiting time - u64.    
-"#
-    )]
-    Raw {
-        #[clap(index = 1, help = "file with sql query")]
-        query_file: PathBuf,
-        #[clap(short, long, help = "name of the process, as it is recorded in /proce/<pid>/comm")]
-        command: Option<String>,
-    },
-}
-
 #[tokio::main]
 async fn main() -> Result<()> {
     let opt = Opt::parse();
@@ -87,23 +60,13 @@ async fn main() -> Result<()> {
     match opt.cmd {
         Command::Pprof {
             destination,
-            cmd,
+            command,
+            query_file,
             binary,
-        } => match cmd {
-            PprofCommand::Cpu { command } => {
-                pprof::pprof(&opt.register, &destination, CPU_PPROF_SQL, Some(&command), binary).await
-            }
-            PprofCommand::Offcpu { command } => {
-                pprof::pprof(&opt.register, &destination, OFFCPU_PPROF_SQL, Some(&command), binary).await
-            }
-            PprofCommand::Rss { command } => {
-                pprof::pprof(&opt.register, &destination, RSS_PPROF_SQL, Some(&command), binary).await
-            }
-            PprofCommand::Raw { query_file, command } => {
-                let query = std::fs::read_to_string(query_file)?;
-                pprof::pprof(&opt.register, &destination, &query, command.as_deref(), binary).await
-            }
-        },
+        } => {
+            let query = std::fs::read_to_string(query_file)?;
+            pprof::pprof(&opt.register, &destination, &query, command.as_deref(), binary).await
+        }
         Command::Trace { destination, queries } => {
             let ctx = session(&opt.register).await?;
             let queries = queries
