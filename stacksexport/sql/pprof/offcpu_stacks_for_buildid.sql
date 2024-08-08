b4bf6c08b05f859e810094660ce42f3ea0633152
Dmitry Shulyak <yashulyak@gmail.com>
2024-08-08 10:02:10 +0200

    add better playbooks to explain what this tool does

---
+++ stacksexport/sql/pprof/offcpu_stacks_for_buildid.sql
@@ -0,0 +1,34 @@
+with offcpu_stacks as (
+    select
+        ustack,
+        kstack,
+        (
+            LEAD(timestamp - duration) OVER (
+                PARTITION BY pid
+                ORDER BY
+                    timestamp - duration
+            )
+        ) - (timestamp) as offcpu
+    from
+        stacks
+    where
+        kind = 'switch'
+        and buildid = decode('?buildid', 'hex')
+        and (
+            not empty(ustack)
+            or not empty(kstack)
+        )
+)
+select
+    array_union(kstack, ustack) as stack,
+    count(*) as count,
+    sum(offcpu) as offcpu_ns
+from
+    offcpu_stacks
+where
+    (
+        not empty(ustack)
+        or not empty(kstack)
+    )
+group by
+    stack
